4bead27ca47ebfa19e2072455fe01234fac682c8
kuanghc1 <kuanghc1@lenovo.com>
2023-02-17 22:57:21 +0800

    Add sql check testing

--- multi-tenancy-library/src/test/java/com/example/multitenancylibrary/sql/TenantSQLQuerySingleTableTest.java
+++ multi-tenancy-library/src/test/java/com/example/multitenancylibrary/sql/TenantSQLQuerySingleTableTest.java
@@ -32,77 +32,81 @@ class TenantSQLQuerySingleTableTest {
         userTable = tables.get(0);
     }
 
-    @Test
-    void testUserTableQuery() {
-        MultiTenancyStorage.setTenantID(4);
+    @Nested
+    @DisplayName("DSL SQL")
+    class DSLSQLTest {
+        @Test
+        void testUserTableQuery() {
+            MultiTenancyStorage.setTenantID(4);
 
-        Result<Record> fetch = dslContext
-                .select()
-                .from(userTable)
-                .fetch();
+            Result<Record> fetch = dslContext
+                    .select()
+                    .from(userTable)
+                    .fetch();
 
-        MultiTenancyStorage.setTenantID(null);
+            MultiTenancyStorage.setTenantID(null);
 
-        Assertions.assertEquals(2, fetch.size());
-    }
+            Assertions.assertEquals(2, fetch.size());
+        }
 
-    @Test
-    void testUserTableQueryWithWhere() {
-        MultiTenancyStorage.setTenantID(4);
+        @Test
+        void testUserTableQueryWithWhere() {
+            MultiTenancyStorage.setTenantID(4);
 
-        Result<Record> fetch = dslContext
-                .select()
-                .from(userTable)
-                .where(userTable.field("tenant_id", Integer.class).eq(4))
-                .fetch();
+            Result<Record> fetch = dslContext
+                    .select()
+                    .from(userTable)
+                    .where(userTable.field("tenant_id", Integer.class).eq(4))
+                    .fetch();
 
-        MultiTenancyStorage.setTenantID(null);
+            MultiTenancyStorage.setTenantID(null);
 
-        Assertions.assertEquals(2, fetch.size());
-    }
+            Assertions.assertEquals(2, fetch.size());
+        }
 
-    @Test
-    void testUserTableQueryWithAlias() {
-        MultiTenancyStorage.setTenantID(4);
+        @Test
+        void testUserTableQueryWithAlias() {
+            MultiTenancyStorage.setTenantID(4);
 
-        Table<?> tUserAlias = userTable.as("t_user_alias");
+            Table<?> tUserAlias = userTable.as("t_user_alias");
 
-        Result<Record> fetch = dslContext
-                .select()
-                .from(tUserAlias)
-                .fetch();
+            Result<Record> fetch = dslContext
+                    .select()
+                    .from(tUserAlias)
+                    .fetch();
 
-        MultiTenancyStorage.setTenantID(null);
+            MultiTenancyStorage.setTenantID(null);
 
-        Assertions.assertEquals(2, fetch.size());
-    }
+            Assertions.assertEquals(2, fetch.size());
+        }
 
-    @Test
-    void testUserTableQueryDistinct() {
-        MultiTenancyStorage.setTenantID(1);
+        @Test
+        void testUserTableQueryDistinct() {
+            MultiTenancyStorage.setTenantID(1);
 
-        Result<? extends Record1<?>> userId = dslContext
-                .selectDistinct(userTable.field("user_id"))
-                .from(userTable)
-                .fetch();
+            Result<? extends Record1<?>> userId = dslContext
+                    .selectDistinct(userTable.field("user_id"))
+                    .from(userTable)
+                    .fetch();
 
-        MultiTenancyStorage.setTenantID(null);
+            MultiTenancyStorage.setTenantID(null);
 
-        Assertions.assertEquals(2, userId.size());
-    }
+            Assertions.assertEquals(2, userId.size());
+        }
 
-    @Test
-    void testUserTableQueryCount() {
-        MultiTenancyStorage.setTenantID(1);
+        @Test
+        void testUserTableQueryCount() {
+            MultiTenancyStorage.setTenantID(1);
 
-        int count = dslContext
-                .selectCount()
-                .from(userTable)
-                .fetchOne(0, int.class);
+            int count = dslContext
+                    .selectCount()
+                    .from(userTable)
+                    .fetchOne(0, int.class);
 
-        MultiTenancyStorage.setTenantID(null);
+            MultiTenancyStorage.setTenantID(null);
 
-        Assertions.assertEquals(3, count);
+            Assertions.assertEquals(3, count);
+        }
     }
 
     @Nested
